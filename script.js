181813d2ec7b4e28b9789d8931487d93a1ff4548
Голиков Богдан Артурович <golikov_ba@edu.surgu.ru>
2025-09-25 13:09:10 +0500

    some

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ function func() {
     var num1 = Number(document.getElementById("myInput1").value);
     var num2 = Number(document.getElementById("myInput2").value);
     result = num1 + num2;
-    document.getElementById("result").innerHTML = result;
+    error = "Одно из введёных значений не является числом!"
+    if (!isNaN(result)) {
+        document.getElementById("result").innerHTML = result;
+    } else if (isNaN(result)) {
+        document.getElementById("result").innerHTML = error;
+    }
 }
