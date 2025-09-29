0e381ae9446f99381fda9aaf75f2859b546da509
godpromise <akuevshi@gmail.com>
2025-09-29 19:14:04 +0500

    some changes

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-function func() {
-    var result
-    var num1 = Number(document.getElementById("myInput1").value);
-    var num2 = Number(document.getElementById("myInput2").value);
-    result = num1 + num2;
-    error = "Одно из введёных значений не является числом!"
-    if (!isNaN(result)) {
-        document.getElementById("result").innerHTML = result;
-    } else if (isNaN(result)) {
-        document.getElementById("result").innerHTML = error;
+const in1 = document.getElementById('myInput1')
+const in2 = document.querySelector('#myInput2')
+const btn = document.getElementById('myButton')
+const out = document.querySelector('#result')
+
+btn.addEventListener('click', (func)=>{
+    
+    func.preventDefault()
+    const num1 = Number(in1.value)
+    const num2 = Number(in2.value)
+    const errors = {
+        one: false,
+        two: false
     }
-}
+    let resultText = ''
+    if  (isNan(num1)){
+        errors.one = true
+    }
+    if (isNan(num2)){
+        errors.two = true
+    }
+    if (errors.one && errors.two){
+        resultText = 'Ошибка: оба значения не являются числом'
+    }
+    if (errors.one&&!errors.two){
+        resultText = 'Ошибка: первое значение не является числом'
+    }
+    if (!errors.one&&errors.two){
+        resultText = 'Ошибка: второе значение не является числом'
+    }
+    if (!errors.one&&!errors.two){
+        const result = num1 + num2;
+        resultText = `Результат: ${num1+num2}`;
+    } 
+
+    out.innerText = resultText
+})
